58c0a5f4ed4479b95f4b28b971bfbb6721f86f10
liangtiao4 <1745941489@qq.com>
2021-10-06 18:09:55 +0800

    mysql config

--- config/db.js
+++ config/db.js
@@ -7,11 +7,11 @@ let MYSQL_CONF
 
 if (env === 'dev') {
   MYSQL_CONF = {
-    host: 'localhost',
-    port: '3306',
-    database: 'test_db',
-    user: 'root',
-    password: 'Daimao,0713'
+    host: 'localhost',    // 主机
+    port: '3306',         // 端口
+    database: 'test_db',  // 数据库名
+    user: 'root',         // 用户
+    password: '123456'    // 密码
   }
 }
 
